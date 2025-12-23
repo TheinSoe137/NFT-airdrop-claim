4da57aa6c4afe0bc81085845bce4cd58ad97f4a5
TheinSoe <thein4edu@gmail.com>
2025-12-23 15:04:40 +0630

    error fix: deployment on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */reactStrictMode: false,
+  serverExternalPackages: ["pino", "thread-stream"],
+  transpilePackages: [
+    "@reown/appkit", 
+    "@reown/appkit-adapter-wagmi", 
+    "@walletconnect/modal",
+    "@walletconnect/ethereum-provider"
+  ],
 };
 
 export default nextConfig;
